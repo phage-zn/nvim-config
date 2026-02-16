d04ec2d3727925376001f241236316dc64a48afa
Luke Swan <lswan@ksg.com>
2026-02-16 16:20:22 +0200

    Update keymap

--- lua/config/keymap-config.lua
+++ lua/config/keymap-config.lua
@@ -17,11 +17,43 @@ return {
   { "<leader>b", "<cmd>Telescope buffers<cr>", desc = "Switch Buffer", mode = "n" },
   { "<leader>u", "<cmd>Telescope undo<cr>", desc = "Undo Tree", mode = "n" },
 
-  { "<A-x>", "<cmd>:lua require(\"mini.bufremove\").delete(0)<cr>", desc = "Delete Current Buffer" },
-  { "<A-X>", "<cmd>1,.-bd | .+1,$bd<cr>", desc = "Delete Other Buffers" },
+  { "<A-x>", "<cmd>bp | bd #<cr>", desc = "Delete Current Buffer" },
+  {
+    "<A-X>",
+    function()
+      local buffers = vim.fn.getbufinfo({ buflisted = 1 })
+      local count = 0
+      local skip_count = 0
+      local notification = "Closed %d buffer(s)."
+      local skip_notification = " Skipped %d modified buffer(s)."
+      for _, buf in ipairs(buffers) do
+        if buf.hidden == 1 then
+          if buf.changed == 1 then
+            local response = vim.fn.input("Close modified buffer '" .. buf.name .. "'? (Y/n): ")
+            if response == "" or response:lower() == "y" then
+              vim.api.nvim_buf_delete(buf.bufnr, { force = true })
+              count = count + 1
+            else
+              vim.notify("Skipped: " .. buf.name)
+              skip_count = skip_count + 1
+            end
+          else
+            vim.api.nvim_buf_delete(buf.bufnr, { force = false })
+            count = count + 1
+          end
+        end
+      end
+      local message = string.format(notification, count)
+      if (skip_count > 0) then
+        message = message .. string.format(skip_notification, skip_count)
+      end
+      vim.notify(message)
+    end,
+    desc = "Delete Other Buffers"
+  },
   { "<A-h>", "<cmd>bp<cr>", desc = "Go to Previous Buffer" },
   { "<A-l>", "<cmd>bn<cr>", desc = "Go to Next Buffer" },
-  { "<A-o>", "<cmd>b#<cr>", desc = "Toggle Last Active Buffer" },
+  { "<A-o>", "<cmd>e#<cr>", desc = "Toggle Last Active Buffer" },
   { "<A-t>", "<cmd>tabnew<cr>", desc = "Create New Tab" },
   { "<A-w>", "<cmd>tabc<cr>", desc = "Close Tab" },
 
@@ -35,10 +67,11 @@ return {
   { "<leader>tf", "<cmd>ToggleTerm direction=float<cr>", desc = "Float" },
   { "<leader>th", "<cmd>ToggleTerm direction=horizontal<cr>", desc = "Horizontal" },
   { "<leader>tv", "<cmd>ToggleTerm direction=vertical<cr>", desc = "Vertical" },
-  { "<F7>",       "<cmd>ToggleTermToggleAll<cr>", desc = "Toggle Terminal",          mode = "nt" },
+  { "<leader>tz", "<cmd>ZenMode<cr>", desc = "Zen Mode" },
+  { "<F7>", "<cmd>ToggleTermToggleAll<cr>", desc = "Toggle Terminal", mode = "nt" },
 
-  { "<leader>g",  group = "Git" },
-  { "<leader>gb", gs.toggle_current_line_blame,   desc = 'Toggle Current Line Blame' },
+  { "<leader>g", group = "Git" },
+  { "<leader>gb", gs.toggle_current_line_blame, desc = 'Toggle Current Line Blame' },
   {
     "<leader>gB",
     function()
@@ -55,16 +88,23 @@ return {
     end,
     desc = "Lazygit"
   },
+  {
+    "<leader>gc",
+    function()
+      gs.show_commit()
+    end,
+    desc = "Show commit"
+  },
 
   -- Vim Built-In Functions
-  { "<Esc>",   "<cmd>nohlsearch<cr>",       desc = "Clear Highlights" },
-  { "<C-s>",   "<cmd>w<cr>",                desc = "Write" },
-  { "<C-s>a",  "<cmd>wa<cr>",               desc = "Write All" },
-  { "<C-q>b",  "<cmd>confirm bd<cr>",       desc = "Confirm Quit Buffer" },
-  { "<C-q>w",  "<cmd>confirm q<cr>",        desc = "Confirm Quit Window" },
-  { "<C-q>q",   "<cmd>confirm qall<cr>",    desc = "Confirm Quit All" },
-  { "<C-q>f",  "<cmd>qa!<cr>",              desc = "Force Quit" },
+  { "<Esc>",  "<cmd>nohlsearch<cr>",   desc = "Clear Highlights" },
+  { "<C-s>",  "<cmd>w<cr>",            desc = "Write" },
+  { "<C-s>a", "<cmd>wa<cr>",           desc = "Write All" },
+  { "<C-q>b", "<cmd>confirm bd<cr>",   desc = "Confirm Quit Buffer" },
+  { "<C-q>w", "<cmd>confirm q<cr>",    desc = "Confirm Quit Window" },
+  { "<C-q>q", "<cmd>confirm qall<cr>", desc = "Confirm Quit All" },
+  { "<C-q>f", "<cmd>qa!<cr>",          desc = "Force Quit" },
 
-  { "|",       "<cmd>vsplit<cr>",           desc = "Vertical Split" },
-  { "\\",      "<cmd>split<cr>",            desc = "Horizontal Split" }
+  { "|",      "<cmd>vsplit<cr>",       desc = "Vertical Split" },
+  { "\\",     "<cmd>split<cr>",        desc = "Horizontal Split" },
 }
