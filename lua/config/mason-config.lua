3f8a313ab74136b3f75d21743516069328ab87db
Luke Swan <lswan@ksg.com>
2026-02-26 16:30:03 +0200

    Mason api update

--- lua/config/mason-config.lua
+++ lua/config/mason-config.lua
@@ -1,62 +1,45 @@
-local servers = {
-  lua_ls = {
-    Lua = {
-      workspace = { checkThirdParty = false },
-      telemetry = { enable = false },
-      hint = { enable = true }
-    }
-  },
-  ts_ls = {
-    typescript = {
-      codeActionsOnSave = {
-        source = {
-          organizeImports = true,
-          fixAll = true,
-          addMissingImports = true
-        }
-      },
-      inlayHints = {
-        includeInlayEnumMemberValueHints = true,
-        includeInlayFunctionLikeReturnTypeHints = true,
-        includeInlayFunctionParameterTypeHints = true,
-        includeInlayParameterNameHints = 'all', -- 'none' | 'literals' | 'all';
-        includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-        includeInlayPropertyDeclarationTypeHints = true,
-        includeInlayVariableTypeHints = true,
-      },
-    },
-  }
-}
-
-local init_options = {
-  ts_ls = {
-    hostInfo = "neovim",
-    preferences = {
-      quotePreference = 'single',
-      includeCompletionsForModuleExports = true,
-      includeCompletionsForImportStatements = true,
-      importModuleSpecifierPreference = 'non-relative',
-      importModuleSpecifierEnding = 'minimal',
-    },
-  },
-  vuels = {}
-}
-
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-
 return {
-  ensure_installed = vim.tbl_keys(servers),
-  handlers = {
-    function(server_name)
-      require('lspconfig')[server_name].setup {
-        capabilities = capabilities,
-        on_attach = require("config.lsp-keymaps").register_lsp_keymaps,
-        settings = servers[server_name],
-        filetypes = (servers[server_name] or {}).filetypes,
-        init_options = init_options[server_name] or {}
-      }
-    end,
-    ['jdtls'] = function()
-    end,
+  servers = {
+    lua_ls = {
+      settings = {
+        Lua = {
+          workspace = { checkThirdParty = false },
+          telemetry = { enable = false },
+          hint = { enable = true }
+        },
+      },
+    },
+    ts_ls = {
+      settings = {
+        typescript = {
+          codeActionsOnSave = {
+            source = {
+              organizeImports = true,
+              fixAll = true,
+              addMissingImports = true
+            }
+          },
+          inlayHints = {
+            includeInlayEnumMemberValueHints = true,
+            includeInlayFunctionLikeReturnTypeHints = true,
+            includeInlayFunctionParameterTypeHints = true,
+            includeInlayParameterNameHints = 'all', -- 'none' | 'literals' | 'all';
+            includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+            includeInlayPropertyDeclarationTypeHints = true,
+            includeInlayVariableTypeHints = true,
+          },
+        },
+      },
+      init_options = {
+        hostInfo = "neovim",
+        preferences = {
+          quotePreference = 'single',
+          includeCompletionsForModuleExports = true,
+          includeCompletionsForImportStatements = true,
+          importModuleSpecifierPreference = 'non-relative',
+          importModuleSpecifierEnding = 'minimal',
+        },
+      },
+    }
   }
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -17,9 +17,24 @@ return {
       },
     },
     config = function()
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      local lsp_capabilities = require("blink.cmp").get_lsp_capabilities(capabilities)
+      vim.lsp.config("*", {
+        capabilities = lsp_capabilities,
+      })
+
       require("mason").setup()
       local mason_config = require("config.mason-config")
-      require("mason-lspconfig").setup(mason_config)
+      require("mason-lspconfig").setup({
+        ensure_installed = vim.tbl_keys(mason_config.servers)
+      })
+
+      for server_name, server_config in pairs(mason_config.servers) do
+        vim.lsp.config(server_name, {
+          settings = server_config.settings,
+          init_options = (server_config.init_options and server_config.init_options[server_name]) or {},
+        })
+      end
     end
   },
   { "mfussenegger/nvim-jdtls" },
