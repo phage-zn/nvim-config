9c17608bb0c59b86fce7b42c225511e5141b3799
Luke Swan <swan.luke9@gmail.com>
2026-03-21 17:41:56 +0200

    Use treesitter for folds

--- lua/config/neovim-config.lua
+++ lua/config/neovim-config.lua
@@ -1,16 +1,16 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 vim.o.termguicolors = true
 vim.o.undofile = true
-vim.o.completeopt = 'menuone,noselect'
 vim.o.showmode = false
 vim.o.scrolloff = 20
 vim.o.breakindent = true
-vim.o.signcolumn = 'yes'
+vim.o.signcolumn = "yes"
 vim.o.cursorline = true
-vim.o.fillchars ="vert:║,horiz:═,vertright:╠,vertleft:╣,horizup:╩,horizdown:╦,verthoriz:╬,eob: "
+vim.o.fillchars = "vert:║,horiz:═,vertright:╠,vertleft:╣,horizup:╩,horizdown:╦,verthoriz:╬,eob: "
 
-vim.opt.foldmethod = 'marker'
+vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.wo[0][0].foldmethod = "expr"
 
 -- Line numbers
 vim.o.number = true
@@ -18,8 +18,9 @@ vim.o.relativenumber = true
 
 -- Tab stop defaults
 vim.o.expandtab = true
-vim.o.shiftwidth = 2
-vim.o.softtabstop = 2
+vim.o.shiftwidth = 4
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
 
 -- Case insensitive search
 vim.o.ignorecase = true
@@ -28,14 +29,14 @@ vim.o.smartcase = true
 vim.o.updatetime = 250
 vim.o.timeoutlen = 300
 
--- Window spit defaults
-vim.o.inccommand = 'split'
+-- Window split defaults
+vim.o.inccommand = "split"
 vim.o.splitright = true
 vim.o.splitbelow = true
 
 -- whitespace
 vim.o.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 
 -- Highlight on search
 vim.o.hlsearch = true
