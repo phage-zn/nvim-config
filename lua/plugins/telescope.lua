94f70ec72ae6cb3ace6c06ef3cd6665eec4f1d5f
Luke Swan <swan.luke9@gmail.com>
2026-03-21 17:42:57 +0200

    upgrade telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,41 +1,40 @@
 return {
   {
-    'nvim-telescope/telescope.nvim',
-    branch = '0.1.x',
-    event = 'VeryLazy',
+    "nvim-telescope/telescope.nvim",
+    version = "*",
+    event = "VeryLazy",
     dependencies = {
-      'nvim-lua/plenary.nvim',
-      'debugloop/telescope-undo.nvim',
+      "nvim-lua/plenary.nvim",
+      "debugloop/telescope-undo.nvim",
       {
-        'nvim-telescope/telescope-fzf-native.nvim',
-        build = 'make',
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
         cond = function()
-          return vim.fn.executable 'make' == 1
+          return vim.fn.executable("make") == 1
         end,
       },
       {
-        'nvim-telescope/telescope-ui-select.nvim'
-      }
+        "nvim-telescope/telescope-ui-select.nvim",
+      },
     },
     config = function()
       local telescopeConfig = {
         pickers = {
           colorscheme = {
-            enable_preview = true
-          }
+            enable_preview = true,
+          },
         },
         extensions = {
-          ['ui-select'] = {
-            require('telescope.themes').get_dropdown(),
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown(),
           },
-          undo = {
-          }
-        }
+          undo = {},
+        },
       }
       require("telescope").setup(telescopeConfig)
-      pcall(require("telescope").load_extension, 'fzf')
-      pcall(require("telescope").load_extension, 'ui-select')
-      pcall(require("telescope").load_extension, 'undo')
-    end
-  }
+      pcall(require("telescope").load_extension, "fzf")
+      pcall(require("telescope").load_extension, "ui-select")
+      pcall(require("telescope").load_extension, "undo")
+    end,
+  },
 }
