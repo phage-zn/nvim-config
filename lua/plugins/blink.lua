66b2b0453d5a1926629a35d232060a56c1ef64ec
Luke Swan <swan.luke9@gmail.com>
2026-03-21 17:42:12 +0200

    Remove chartoggle

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,20 +1,17 @@
 return {
   {
-    'saghen/blink.chartoggle',
-  },
-  {
-    'saghen/blink.cmp',
-    dependencies = { 'rafamadriz/friendly-snippets' },
+    "saghen/blink.cmp",
+    dependencies = { "rafamadriz/friendly-snippets" },
     version = "1.*",
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
     opts = {
       keymap = {
-        preset = 'default',
+        preset = "default",
       },
 
       appearance = {
-        nerd_font_variant = 'mono',
+        nerd_font_variant = "mono",
       },
 
       completion = {
@@ -23,11 +20,11 @@ return {
       },
 
       sources = {
-        default = { 'lsp', 'path', 'snippets', 'buffer' },
+        default = { "lsp", "path", "snippets", "buffer" },
       },
 
-      fuzzy = { implementation = "prefer_rust_with_warning" }
+      fuzzy = { implementation = "prefer_rust_with_warning" },
     },
-    opts_extend = { "sources.default" }
-  }
+    opts_extend = { "sources.default" },
+  },
 }
