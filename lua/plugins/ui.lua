150c2801390eddcc63fc2f88e11bbc5979d88976
Luke Swan <swan.luke9@gmail.com>
2026-03-21 17:39:38 +0200

    Remove unused plugins

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -58,14 +58,6 @@ return {
       vim.cmd.colorscheme("catppuccin-mocha")
     end,
   },
-  -- { "folke/tokyonight.nvim", config = function() vim.cmd.colorscheme "tokyonight" end },
-  {
-    "zenbones-theme/zenbones.nvim",
-    -- Optionally install Lush. Allows for more configuration or extending the colorscheme
-    -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
-    -- In Vim, compat mode is turned on as Lush only works in Neovim.
-    dependencies = "rktjmp/lush.nvim",
-  },
   {
     "lewis6991/gitsigns.nvim",
     config = function()
