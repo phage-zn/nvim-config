c9ae2a14ddca08492092e047febdb181bb130143
Luke Swan <lswan@ksg.com>
2026-04-14 18:40:59 +0300

    Telescope stuff

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -22,6 +22,10 @@ return {
         colorscheme = {
           enable_preview = true,
         },
+        find_files = {
+          -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+          find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+        },
       },
       extensions = {
         ["ui-select"] = function()
@@ -33,6 +37,16 @@ return {
       },
     },
     config = function(_, opts)
+      local telescopeConfig = require("telescope.config")
+
+      local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+
+      table.insert(vimgrep_arguments, "--hidden")
+      table.insert(vimgrep_arguments, "--glob")
+      table.insert(vimgrep_arguments, "!**/.git/*")
+      opts.defaults = {
+        vimgrep_arguments = vimgrep_arguments,
+      }
       require("telescope").setup(opts)
       pcall(require("telescope").load_extension, "fzf")
       pcall(require("telescope").load_extension, "ui-select")
